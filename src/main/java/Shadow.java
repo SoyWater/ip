75816b40267e76ad10d354ddc496b206439cfa71
SoyWater <ivanlxy123@gmail.com>
2025-08-21 16:36:29 +0800

    Add divider for seperation

--- src/main/java/Shadow.java
+++ src/main/java/Shadow.java
@@ -1,5 +1,6 @@
-public class Duke {
+public class Shadow {
     public static void main(String[] args) {
+        String divider = "_________________________________________________________";
         String asciiArt =
                 """
                          ____  _               _              \s
@@ -9,8 +10,10 @@ public class Duke {
                         |____/|_| |_|\\__,_|\\__,_|\\___/ \\_/\\_/ \s
                 """;
 
+        System.out.println(divider);
         System.out.println("Hello, this is \n" + asciiArt);
         System.out.println("What's your demand today?");
+        System.out.println(divider);
 
     }
 }

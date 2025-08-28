24cbbf320a687af407594d51279fc51b65f54ab9
SoyWater <ivanlxy123@gmail.com>
2025-08-29 00:14:02 +0800

    Add TextUi class

--- src/main/java/Shadow.java
+++ src/main/java/Shadow.java
@@ -1,38 +1,17 @@
-import java.util.*;
-import java.util.function.Consumer;
-
 public class Shadow {
-
     public static void main(String[] args) {
-        Runnable printDivider = () -> System.out.println("_________________________________________________________");
-        String asciiArt =
-                """
-                         ____  _               _
-                        / ___|| |__   __ _  __| | _____      __
-                        \\___ \\| '_ \\ / _` |/ _` |/ _ \\ \\ /\\ / /
-                         ___) | | | | (_| | (_| | (_) \\ V  V /
-                        |____/|_| |_|\\__,_|\\__,_|\\___/ \\_/\\_/
-                
-                """;
-        String sayHello = "Hello, this is\n" + asciiArt + "What's your demand today?";
-        String sayBye = "Very well, contact me again when you have more demands.";
-        printDivider.run();
-        System.out.println(sayHello);
-
-        Scanner userInput = new Scanner(System.in);
+        Ui.getInstance().sayHello();
         while (true) {
-            printDivider.run();
-            System.out.print("> ");
-
-            String demand = userInput.nextLine();
+            Ui.getInstance().printLine();
+            Ui.getInstance().printCommandInput();
+            String demand = Ui.getInstance().readDemand();
             if (demand.toLowerCase().startsWith("bye")) {
-                System.out.println(sayBye);
+                Ui.getInstance().sayBye();
                 break;
             }
             Parser.parse(demand);
-
         }
-        userInput.close();
+        Ui.getInstance().closeReader();
     }
 }
 
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ui {
+    private static Ui ui;
+    private final Scanner userInput;
+
+    private Ui() {
+         this.userInput = new Scanner(System.in);
+    }
+
+    public static Ui getInstance() {
+        if (Ui.ui == null) {
+            Ui.ui = new Ui();
+        }
+        return Ui.ui;
+    }
+
+    public String readDemand() {
+        return userInput.nextLine();
+    }
+
+    public void closeReader() {
+        userInput.close();
+    }
+
+    public void sayHello() {
+        String asciiArt =
+                """
+                         ____  _               _
+                        / ___|| |__   __ _  __| | _____      __
+                        \\___ \\| '_ \\ / _` |/ _` |/ _ \\ \\ /\\ / /
+                         ___) | | | | (_| | (_| | (_) \\ V  V /
+                        |____/|_| |_|\\__,_|\\__,_|\\___/ \\_/\\_/
+                
+                """;
+        String sayHello = "Hello, this is\n" + asciiArt + "What's your demand today?";
+        this.printLine();
+        System.out.println(sayHello);
+    }
+
+    public void printLine() {
+        System.out.println("_________________________________________________________");
+    }
+
+    public void sayBye() {
+        String sayBye = "Very well, contact me again when you have more demands.";
+        System.out.println(sayBye);
+    }
+
+    public void printCommandInput() {
+        System.out.print("> ");
+    }
 }

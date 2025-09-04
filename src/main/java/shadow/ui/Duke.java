c03484d947f8da80ea2dfeab5f23b8121ae09a0d
SoyWater <ivanlxy123@gmail.com>
2025-09-04 22:47:26 +0800

    Add skeleton for chat ui

---
+++ src/main/java/shadow/ui/Duke.java
@@ -0,0 +1,11 @@
+package shadow.ui;
+
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+}
---
+++ src/main/java/shadow/ui/MainWindow.java
@@ -0,0 +1,45 @@
+package shadow.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duke duke;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setDuke(Duke duke) {
+        this.duke = duke;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userTextInput = userInput.getText();
+        String dukeResponse = duke.getResponse(userTextInput);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userTextInput, userImage),
+                DialogBox.getDukeDialog(dukeResponse, dukeImage)
+        );
+        userInput.clear();
+    }
+}
